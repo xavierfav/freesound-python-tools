eeb15183a50c0aa9df07e47cba0757053ca5cf1b
xavier <xavier.favory@gmail.com>
2016-10-27 12:46:53 +0200

    Added TfidfEmbeddingVectorizer

--- manager.py
+++ manager.py
@@ -37,6 +37,9 @@ import re
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from stop_words import get_stop_words
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models.word2vec import Word2Vec
+
 
 LENGTH_BAR = 30 # length of the progress bar
 
@@ -1076,6 +1079,33 @@ class Basket:
         
         return [TaggedDocument(words, tags) for words, tags in zip(all_descriptions, all_tags)]
         
+    class TfidfEmbeddingVectorizer(object):
+        def __init__(self, word2vec):
+            self.word2vec = word2vec
+            self.word2weight = None
+            self.dim = len(word2vec.itervalues().next())
+
+        def fit(self, X, y):
+            tfidf = TfidfVectorizer(analyzer=lambda x: x)
+            tfidf.fit(X)
+            # if a word was never seen - it must be at least as infrequent
+            # as any of the known words - so the default idf is the max of 
+            # known idf's
+            max_idf = max(tfidf.idf_)
+            self.word2weight = defaultdict(
+                lambda: max_idf,
+                [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+            return self
+
+        def transform(self, X):
+            return np.array([
+                    np.mean([self.word2vec[w] * self.word2weight[w]
+                             for w in words if w in self.word2vec] or
+                            [np.zeros(self.dim)], axis=0)
+                    for words in X
+                ])
+
 #_________________________________________________________________#
 #                           NLP class                             #
 #_________________________________________________________________#
